60a8ae1c016f9c62ff968d71dc6a1452d1cce2f9
King.Wang <xiren_88@126.com>
2017-08-24 00:02:24 +0800

    remove oauth component

--- api-gateway/src/main/java/com/example/api/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/api/gateway/ApiGatewayApplication.java
@@ -2,35 +2,15 @@ package com.example.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
-@EnableOAuth2Sso
-@EnableOAuth2Client
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
-    }
-
-    @Configuration
-    public static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable();
-            http.authorizeRequests()
-                    .antMatchers("/userService/**")
-                    .permitAll()
-                    .anyRequest().authenticated();
-        }
     }
 }
--- service-consumer/src/main/java/com/example/service/consumer/ServiceConsumerApplication.java
+++ service-consumer/src/main/java/com/example/service/consumer/ServiceConsumerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
